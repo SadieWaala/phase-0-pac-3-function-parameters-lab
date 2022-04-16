5ed236712a41e3cc8c23876d6c52fbb2a4f40134
Sadie Waala <sadie.waala@yahoo.com>
2022-04-16 15:32:31 -0400

    commit tree 37d7d4d1d14efed7940f7b5335247903b3eb350d

---
+++ index.js
@@ -0,0 +1,12 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+}
+
+function introduction(firstName) {
+    return `Hi, my name is ${firstName}.`
+}
+
